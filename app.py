6032056b66075240e6e163d96d8303b6a7288a9a
Nelson Murerwa <murerwanelson31@gmail.com>
2026-05-23 12:20:03 +0200

    Add deployment configuration for Render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 from tmdb_clustering import (
@@ -49,4 +50,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
